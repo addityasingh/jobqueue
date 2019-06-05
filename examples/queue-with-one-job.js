1d10caf7087627737d7e8ef78fc5f84b5eb20b06
Aditya Pratap Singh <adisinghrajput@gmail.com>
2019-06-05 22:37:49 +0200

    feat: update readme and example with single concurrency (#3)

---
+++ examples/queue-with-one-job.js
@@ -0,0 +1,29 @@
+const JobQueue = require("../dist/index").default;
+const { JobStackFullError, TimeoutError } = require("../dist/index");
+
+const dummyFetch = () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve("success"), 1000);
+  });
+let cancelledJobs = 0;
+let timedOutJobs = 0;
+
+// Initialize the job queue
+const queue = new JobQueue({});
+const jobs = Array.from({ length: 2 }).map(() => dummyFetch);
+
+jobs.forEach(async job => {
+  const err = await queue.execute(job);
+  if (err instanceof JobStackFullError) {
+    cancelledJobs += 1;
+  }
+
+  if (err instanceof TimeoutError) {
+    timedOutJobs += 1;
+  }
+});
+
+queue.flush();
+
+console.log(">>>>>>Timed out jobs", timedOutJobs);
+console.log(">>>>>>Cancelled jobs", cancelledJobs);
